a5b5598dba3a86aab6bf1433412dcf26955dc622
Carolina Rodrigues <rodriguesbcarolina@gmail.com>
2023-11-27 22:19:33 +0000

    solved iterations 1-5

--- src/components/AddFoodForm.jsx
+++ src/components/AddFoodForm.jsx
@@ -1,1 +1,83 @@
-// Your code here
+import { useState } from 'react';
+
+// Iteration 5: Add New Food
+
+function AddFoodForm({ addFood }) {
+  // props = food
+  // properties: id, name, image, calories, servings
+
+  const [name, setName] = useState('');
+  const [image, setImage] = useState('');
+  const [calories, setCalories] = useState(0);
+  const [servings, setServings] = useState(0);
+
+  // Handler functions
+  const handleName = e => setName(e.target.value);
+  const handleImage = e => setImage(e.target.value);
+  const handleCalories = e => setCalories(e.target.value);
+  const handleServings = e => setServings(e.target.value);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    const newFood = {
+      id: Math.random(),
+      name,
+      image,
+      calories,
+      servings,
+    };
+
+    addFood(newFood);
+
+    setName('');
+    setImage('');
+    setCalories(0);
+    setServings(0);
+  };
+
+  return (
+    <div>
+      <h2>Add Food</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor='name'>Name:</label>
+        <input
+          type='text'
+          name='name'
+          id='name'
+          value={name}
+          onChange={handleName}
+        />
+        <label htmlFor='image'>Image:</label>
+        <input
+          type='text'
+          name='image'
+          id='image'
+          value={image}
+          onChange={handleImage}
+        />
+        <label htmlFor='calories'>Calories:</label>
+        <input
+          type='number'
+          name='calories'
+          id='calories'
+          min={0}
+          value={calories}
+          onChange={handleCalories}
+        />
+        <label htmlFor='servings'>Servings:</label>
+        <input
+          type='number'
+          name='servings'
+          id='servings'
+          min={0}
+          value={servings}
+          onChange={handleServings}
+        />
+        <button type='submit'>Create</button>
+      </form>
+    </div>
+  );
+}
+
+export default AddFoodForm;
--- src/components/FoodBox.jsx
+++ src/components/FoodBox.jsx
@@ -1,1 +1,26 @@
-// Your code here
+// Iteration 2: FoodBox component
+function FoodBox({ food, deleteFood }) {
+  // props = food
+
+  return (
+    <div>
+      <p>{food.name}</p>
+
+      <img src={food.image} />
+
+      <p>Calories: {food.calories}</p>
+      <p>
+        Servings <b>{food.servings}</b>
+      </p>
+
+      <p>
+        <b>Total Calories: {food.servings * food.calories}</b> kcal
+      </p>
+      {/* Iteration 4: Delete List items */}
+      {/* Using the delete function received from the App component */}
+      <button onClick={() => deleteFood(food.id)}>Delete</button>
+    </div>
+  );
+}
+
+export default FoodBox;
